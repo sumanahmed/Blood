0f86e4574aef343744fed2561577369742c4d66a
sumanahmed <suman777333@gmail.com>
2020-11-06 11:30:12 +0600

    menu added to show blog page

--- resources/views/blood/frontend/includes/header.blade.php
+++ resources/views/blood/frontend/includes/header.blade.php
@@ -35,7 +35,7 @@
                         <li><a href="{{ route('frontend.campaign') }}">Campaigns</a></li>
                         <li><a href="{{ route('frontend.faq') }}" title="FAQ">FAQ</a></li>
                         <li><a href="{{ route('frontend.gallery') }}" title="Gallery">Gallery</a></li>
-                        <li><a href="blog.html">All Posts</a></li> 
+                        <li><a href="{{ route('frontend.blog') }}">Blog</a></li> 
                         <li><a href="contact.html">Contact</a></li>
                     </ul>
                 </div>
