85cbade75031554c326a34a469b470a6903b8cf0
sumanahmed <suman777333@gmail.com>
2020-11-11 23:35:02 +0600

    different method modify to show frontend different page content

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Campaign;
+use App\Models\Gallery;
+use App\Models\Faq;
+use App\Models\Category;
+use App\Models\Blog;
 
 class HomeController extends Controller
 {
     //show home page
     public function home(){ 
-        return view("blood.frontend.home.index");
+        $campaigns = Campaign::where('status',1)->orderBy('id','DESC')->get();
+        $gallerys  = Gallery::orderBy('id','DESC')->get();
+        return view("blood.frontend.home.index",compact('campaigns','gallerys'));
     }
     //show about page
     public function about(){ 
@@ -21,15 +28,20 @@ class HomeController extends Controller
     }
     //show faq page
     public function faq(){ 
-        return view("blood.frontend.home.faq");
+        $faqs = Faq::orderBy('id','desc')->get();
+        return view("blood.frontend.home.faq",\compact('faqs'));
     }
     //show gallery page
     public function gallery(){ 
-        return view("blood.frontend.home.gallery");
+        $gallerys  = Gallery::orderBy('id','DESC')->get();
+        return view("blood.frontend.home.gallery",compact('gallerys'));
     }
     //show blog page
     public function blog(){ 
-        return view("blood.frontend.home.blog");
+        $categories   = Category::all();
+        $blogs        = Blog::all();
+        $recent_blogs = Blog::orderBy('id','desc')->limit(3)->get();
+        return view("blood.frontend.home.blog",compact('categories','blogs','recent_blogs'));
     }
     //show contact page
     public function contact(){ 
