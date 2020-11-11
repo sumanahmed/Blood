8a999d161d976d3fddc3453863a980b0ef2799be
sumanahmed <suman777333@gmail.com>
2020-11-11 23:34:38 +0600

    blog page modify to show blog

--- resources/views/blood/frontend/home/blog.blade.php
+++ resources/views/blood/frontend/home/blog.blade.php
@@ -17,29 +17,29 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-sm-12">
-                    <article class="post single-post">
-                        <div class="single-post-content">
-                            <a title="" href="#">
-                                <img alt="" src="{{ asset('blood/frontend/images/blog_1.jpg') }}" />
-                            </a>
-                        </div> <!-- end .bd-view  -->
-                        <div class="single-post-title">
-                            <h2>
-                                <a href="#">
-                                    Blood Connects Us All in a Soul.Let's donate without hesitation.
+                    @foreach($blogs as $blog)
+                        <article class="post single-post">
+                            <div class="single-post-content">
+                                <a title="" href="#">
+                                    <img alt="" src="{{ asset($blog->thumbnail) }}" />
                                 </a>
-                            </h2> 
-                            <p class="single-post-meta">
-                                <i class="fa fa-user"></i>
-                                &nbsp;Deborah Beck
-                                &nbsp;<i class="fa fa-book"></i>
-                                &nbsp;<a title="View all posts" href="#"> Relation </a>
-                                &nbsp;<i class="fa fa-calendar"></i>
-                                &nbsp;January 19, 2016
-                            </p>
-                            <p>We have adopted the slogan “Share life, give blood”, to draw attention to the roles that voluntary donation systems play in encouraging people to care for one another and promote community cohesion.</p>
-                        </div> <!-- end col-sm-8  -->
-                    </article>
+                            </div> <!-- end .bd-view  -->
+                            <div class="single-post-title">
+                                <h2>
+                                    <a href="#">{{ $blog->title }}</a>
+                                </h2> 
+                                <p class="single-post-meta">
+                                    <i class="fa fa-user"></i>
+                                    &nbsp;Deborah Beck
+                                    &nbsp;<i class="fa fa-book"></i>
+                                    &nbsp;<a title="View all posts" href="#"> Relation </a>
+                                    &nbsp;<i class="fa fa-calendar"></i>
+                                    &nbsp;January 19, 2016
+                                </p>
+                                <p>{{ $blog->description }}</p>
+                            </div> <!-- end col-sm-8  -->
+                        </article>
+                    @endforeach
                     <div class="blog-pagination text-center clearfix">    
                         <ul class="pagination">
                             <li><a href="#" class="prev page-numbers"><i class="fa fa-caret-left"></i></a></li>
@@ -64,47 +64,25 @@
                     </div> <!--  end .widget -->      
                     <div class="widget site-sidebar">
                         <h2 class="widget-title">Categories</h2>
-                        <ul class="widget-post-category clearfix">                            <li>
-                                <a title="View all posts filed under Environment" href="#">Blood Relation</a>
-                                <span class="pull-right badge">42</span>
-                            </li>
-                            <li>
-                                <a title="View all posts filed under Technology" href="#">AB- Blood donor</a>
-                                <span class="pull-right badge">40</span>
-                            </li>
-                            <li>
-                                <a title="View all posts filed under Health" href="#">Keep Safe Blood</a>
-                                <span class="pull-right badge">32</span>
-                            </li>
-                            <li>
-                                <a title="View all posts filed under Disaster" href="#">Preserve Blood</a>
-                                <span class="pull-right badge">26</span>
-                            </li>
-                            <li>
-                                <a title="View all posts filed under Environment Right" href="#">Donation center</a>
-                                <span class="pull-right badge">18</span>
-                            </li>
-                            <li>
-                                <a title="View all posts filed under Education" href="#">Proper Education</a>
-                                <span class="pull-right badge">12</span>
-                            </li>
+                        <ul class="widget-post-category clearfix">                            
+                            @foreach($categories as $category)
+                                <li>
+                                    <a title="View all posts filed under Environment" href="#">{{ $category->name }}</a>
+                                    @php  $blog_count = \App\Models\Blog::where('category_id',$category->id)->count('id'); @endphp
+                                    <span class="pull-right badge">{{ $blog_count }}</span>
+                                </li>
+                            @endforeach
                         </ul>
                     </div> <!--  end .widget -->
 
                     <div class="widget site-sidebar">
                         <h2 class="widget-title">Recent Posts</h2>
-                        <div class="single-recent-post">
-                            <a href="#">Zomato Commits to Making Food Delivery</a>
-                            <span><i class="fa fa-calendar icon-color"></i>&nbsp; April 19, 2017</span>
-                        </div>
-                        <div class="single-recent-post">
-                            <a href="#">Make Kalam's House A Knowledge Centre</a>
-                            <span><i class="fa fa-calendar icon-color"></i>&nbsp; April 18, 2017</span>
-                        </div>
-                        <div class="single-recent-post">
-                            <a href="#">Central Government Retracts Proposal</a>
-                            <span><i class="fa fa-calendar icon-color"></i>&nbsp; April 17, 2017</span>
-                        </div>
+                        @foreach($recent_blogs as $recent_blog)
+                            <div class="single-recent-post">
+                                <a href="#">{{ $recent_blog->title }}</a>
+                                <span><i class="fa fa-calendar icon-color"></i>&nbsp; {{ date('j M, Y', strtotime($recent_blog->title)) }}</span>
+                            </div>
+                        @endforeach
                     </div>
                 </div> 
             </div> 
