485788033e74b3131c8c566d8bb3ab1b4d423d78
sumanahmed <suman777333@gmail.com>
2020-11-11 23:34:24 +0600

    faq page modify to show faq

--- resources/views/blood/frontend/home/faq.blade.php
+++ resources/views/blood/frontend/home/faq.blade.php
@@ -25,111 +25,19 @@
         </div> 
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="accordion">
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" >What are preliminary psychological disorder ?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseOne" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
+                @foreach($faqs as $faq)
+                    <div class="panel panel-default faq-box">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $faq->id }}" >{{ $faq->question }}</a>
+                            </h4>
+                        </div>
+                        <div id="collapse{{ $faq->id }}" class="panel-collapse collapse">
+                            <div class="panel-body"> {{ $faq->answer }}</div>
                         </div>
                     </div>
-                </div>
-
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">How many sessions can I have with my counselor?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseTwo" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
-                        </div>
-                    </div>
-                </div>
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree"> How can I be sure effective form of counseling?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseThree" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
-                        </div>
-                    </div>
-                </div>
-
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" >How is my privacy protected?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseFour" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
-                        </div>
-                    </div>
-                </div> 
+                @endforeach
             </div> <!-- end .col-md-6  -->
-
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="accordion2">
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseFive" >What are preliminary psychological disorder ?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseFive" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
-                        </div>
-                    </div>
-                </div>
-
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseSix">How many sessions can I have with my counselor?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseSix" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
-                        </div>
-                    </div>
-                </div>
-
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseSeven"> How can I be sure effective form of counseling?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseSeven" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
-                        </div>
-                    </div>
-                </div>
-
-                <div class="panel panel-default faq-box">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                            <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseEight" >How is my privacy protected?</a>
-                        </h4>
-                    </div>
-                    <div id="collapseEight" class="panel-collapse collapse">
-                        <div class="panel-body">
-                            We all to some extent are on an End of Life journey. For some of our clients and their family additional care and help is required as they progress through this journey. Care on Call work closely with.
-                        </div>
-                    </div>
-                </div> 
-            </div> <!-- end .col-md-12  -->  
         </div> <!-- end .row  -->
     </div> 
 </section> 
