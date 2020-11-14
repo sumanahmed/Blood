dfde3bef72c120ada8d5b91787b8fd034bb3b25e
sumanahmed <suman777333@gmail.com>
2020-11-14 22:34:12 +0600

    eloquent relation created with division

--- app/Models/District.php
+++ app/Models/District.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class District extends Model
 {
-    //
+    //eloquent relation with division
+    public function division(){
+        return $this->belongsTo('App\Models\Division','division_id');
+    }
 }
