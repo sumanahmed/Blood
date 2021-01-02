645d16425779792814cf84af87162599c838f95a
sumanahmed <suman777333@gmail.com>
2021-01-02 20:35:54 +0600

    modify for logged user info shows

--- resources/views/blood/frontend/donor/dashboard.blade.php
+++ resources/views/blood/frontend/donor/dashboard.blade.php
@@ -5,9 +5,18 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 text-center">
-                    <h3>Welcome to dashboard</h3>
+                    <h3>Donor dashboard</h3>
                 </div>
             </div> <!-- end .row  -->
         </div> <!-- end .container  -->
     </section> <!-- end .page-header  -->
+    <section class="main-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <span>Dear <strong>{{ Auth::guard('donor')->user()->name }}</strong>, welcome to your dashboard</span>
+                </div> 
+            </div> 
+        </div> 
+    </section>
 @endsection
