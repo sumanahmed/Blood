09fe9bb9ee49bb87d82ce4b713447690aab6e569
sumanahmed <suman777333@gmail.com>
2021-01-02 20:41:09 +0600

    modify for dono guard added

--- app/Models/Donor.php
+++ app/Models/Donor.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class Donor extends Model
+class Donor extends Authenticatable
 {
+    use Notifiable;
+
+    protected $guard = 'donor';
+
+    protected $fillable = [
+        'name', 'phone', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
     //eloquent relation with blood group
     public function bloodGroup(){
         return $this->belongsTo('App\Models\BloodGroup','blood_group_id');
