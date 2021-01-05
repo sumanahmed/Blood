218dca72a918690997136a4eedfe1b14b9616da1
sumanahmed <suman777333@gmail.com>
2021-01-05 06:52:48 +0600

    modify for faq breadcumg

--- resources/views/blood/frontend/home/faq.blade.php
+++ resources/views/blood/frontend/home/faq.blade.php
@@ -24,7 +24,7 @@
             </div>
         </div> 
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" id="accordion">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="accordion">
                 @foreach($faqs as $faq)
                     <div class="panel panel-default faq-box">
                         <div class="panel-heading">
