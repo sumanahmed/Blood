4213c1c37b2d3ed64e74321dd2a6cbbe6d3aaf72
sumanahmed <suman777333@gmail.com>
2021-01-05 07:01:16 +0600

    modify for map add

--- resources/views/blood/frontend/home/contact.blade.php
+++ resources/views/blood/frontend/home/contact.blade.php
@@ -22,7 +22,7 @@
                 <ul class="contact-info">
                     <li>
                         <span class="icon-container"><i class="fa fa-home"></i></span>
-                        <address>3100 C/A Mouchak,Sylhet,UK</address>
+                        <address>House#4, Banani, Rd No 1, Dhaka</address>
                     </li>
                 </ul> 
             </div>
@@ -30,7 +30,7 @@
                 <ul class="contact-info">
                     <li>
                         <span class="icon-container"><i class="fa fa-phone"></i></span>
-                        <address><a href="#">+093-120-525-9162</a></address>
+                        <address><a href="#">+880 185 095 9226</a></address>
                     </li>
                 </ul>     
             </div>
@@ -39,7 +39,7 @@
                 <ul class="contact-info">
                     <li>
                         <span class="icon-container"><i class="fa fa-envelope"></i></span>
-                        <address><a href="mailto:">query@yourdomain.com</a></address>
+                        <address><a href="mailto:">support@blooddonation.com</a></address>
                     </li>
                 </ul>    
             </div>
@@ -48,7 +48,7 @@
                 <ul class="contact-info">
                     <li>
                         <span class="icon-container"><i class="fa fa-globe"></i></span>
-                        <address><a href="#">www.yourdomain.com</a></address>
+                        <address><a href="#">www.bloodomation.com</a></address>
                     </li>
                 </ul>     
             </div>      
@@ -87,7 +87,9 @@
             <div class="col-sm-6 wow fadeInRight">
                 <h2 class="contact-title">Our Location</h2>
                 <div class="section-google-map-block wow fadeInUp">
-                    <div id="map_canvas"></div>
+                    <div id="map_canvas">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.866752788142!2d90.39872161429771!3d23.787758793266516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c712dea569b3%3A0x73a5f6509b2106b6!2sDhaka%20International%20University%20Campus%201!5e0!3m2!1sen!2sbd!4v1609808077191!5m2!1sen!2sbd" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
                 </div> 
             </div> 
         </div> 
