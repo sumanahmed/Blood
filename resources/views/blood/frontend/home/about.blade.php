117a64f694bcb74cc8397bd99b9e0c07c0371c0b
sumanahmed <suman777333@gmail.com>
2021-01-05 06:47:35 +0600

    modify for content add

--- resources/views/blood/frontend/home/about.blade.php
+++ resources/views/blood/frontend/home/about.blade.php
@@ -36,102 +36,32 @@
         </div> <!-- end .row  -->
 
         <div class="row">
+            @foreach($volunters as $volunter)
+                <div class="col-lg-4 col-md-offset-0 col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+                    <div class="team-layout-1">
+                        <figure class="team-member">
+                            <a href="#" title="ALEXANDER GARY">
+                                <img src="{{ asset($volunter->thumbnail) }}" alt="{{ $volunter->name }}"/>
+                            </a>
+                        </figure> <!-- end. team-member  -->
 
-            <div class="col-lg-4 col-md-offset-0 col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+                        <article class="team-info">
+                            <h3>{{ $volunter->name }}</h3>                                   
+                            <h4>{{ $volunter->designation }}</h4>
+                        </article>
 
-                <div class="team-layout-1">       
-
-                    <figure class="team-member">
-                        <a href="#" title="ALEXANDER GARY">
-                            <img src="{{ asset('blood/frontend/images/team_9.jpg') }}" alt="ALEXANDER GARY"/>
-                        </a>
-
-                    </figure> <!-- end. team-member  -->
-
-                    <article class="team-info">
-                        <h3>ALEXANDER GARY</h3>                                   
-                        <h4>Co-Founder</h4>
-                    </article>
-
-                    <div class="team-content">
-
-                        <div class="team-social-share text-center clearfix">
-                            <a class="fa fa-facebook rectangle" href="#" title="Facebook"></a>
-                            <a class="fa fa-twitter rectangle" href="#" title="Twitter"></a>
-                            <a class="fa fa-google-plus rectangle" href="#" title="Google Plus"></a>
-                            <a class="fa fa-linkedin rectangle" href="#" title="Linkedin"></a>
-                        </div> <!-- end .author-social-box  -->
-
-                    </div>                             
-
-                </div> <!--  end team-layout-1 -->
-
-            </div> <!--  end .col-md-4 col-sm-12  -->
-
-            <div class="col-lg-4 col-md-offset-0 col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
-
-                <div class="team-layout-1">       
-
-                    <figure class="team-member">
-                        <a href="#" title="MELISSA MUNOZ">
-                            <img src="{{ asset('blood/frontend/images/team_6.jpg') }}" alt="MELISSA MUNOZ" />
-                        </a>
-
-                    </figure> <!-- end. team-member  -->
-
-                    <article class="team-info">
-                        <h3>MELISSA MUNOZ</h3>                                   
-                        <h4>Founder</h4>
-                    </article>
-
-                    <div class="team-content">
-
-                        <div class="team-social-share text-center clearfix">
-                            <a class="fa fa-facebook rectangle" href="#" title="Facebook"></a>
-                            <a class="fa fa-twitter rectangle" href="#" title="Twitter"></a>
-                            <a class="fa fa-google-plus rectangle" href="#" title="Google Plus"></a>
-                            <a class="fa fa-linkedin rectangle" href="#" title="Linkedin"></a>
-                        </div> <!-- end .author-social-box  -->
-
-                    </div>                             
-
-                </div> <!--  end team layout-1 -->
-
-            </div> <!--  end .col-md-4 col-sm-12  -->
-
-            <div class="col-lg-4 col-md-offset-0 col-md-4 col-md-offset-0 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
-
-                <div class="team-layout-1">       
-
-                    <figure class="team-member">
-                        <a href="#" title="JOHN ABRAHAM">
-                            <img src="{{ asset('blood/frontend/images/team_7.jpg') }}" alt="JOHN ABRAHAM" />
-                        </a>                               
-
-                    </figure> <!-- end. team-member  -->
-
-                    <article class="team-info">
-                        <h3>JOHN ABRAHAM</h3>                                   
-                        <h4>Manager</h4>
-                    </article>
-                    <div class="team-content">
-
-                        <div class="team-social-share text-center clearfix">
-                            <a class="fa fa-facebook rectangle" href="#" title="Facebook"></a>
-                            <a class="fa fa-twitter rectangle" href="#" title="Twitter"></a>
-                            <a class="fa fa-google-plus rectangle" href="#" title="Google Plus"></a>
-                            <a class="fa fa-linkedin rectangle" href="#" title="Linkedin"></a>
-                        </div> <!-- end .author-social-box  -->
-
-                    </div>                             
-
-                </div> <!--  end team-layout-1 -->
-
-            </div> <!--  end .col-md-4 col-sm-12  -->  
-
-
+                        <div class="team-content">
+                            <div class="team-social-share text-center clearfix">
+                                <a class="fa fa-facebook rectangle" href="#" title="Facebook"></a>
+                                <a class="fa fa-twitter rectangle" href="#" title="Twitter"></a>
+                                <a class="fa fa-google-plus rectangle" href="#" title="Google Plus"></a>
+                                <a class="fa fa-linkedin rectangle" href="#" title="Linkedin"></a>
+                            </div> <!-- end .author-social-box  -->
+                        </div>
+                    </div> <!--  end team-layout-1 -->
+                </div> <!--  end .col-md-4 col-sm-12  -->
+            @endforeach
         </div> <!-- end .row  --> 
-
     </div>
 </section> 
 
@@ -202,73 +132,20 @@
             <div class="col-md-12 col-sm-12 text-center">
                 <h2 class="section-heading">Our Sponsors</h2>
                 <p class="section-subheading">The sponsors who give their valuable amount to fulfill our mission.</p>
-            </div> <!-- end .col-sm-10  -->                      
+            </div> <!-- end .col-sm-10  -->                     
 
         </div> <!-- end .row  -->
 
 
         <div class="row">
-
             <div class="logo-items logo-layout-1 text-center">
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_1.jpg') }}" alt="" />
-
-                </div>
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_2.jpg') }}" alt="" />
-
-                </div>
-
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_3.jpg') }}" alt="" />
-
-                </div>
-
-
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_4.jpg') }}" alt="" />
-
-                </div>
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_5.jpg') }}" alt="" />
-
-                </div>
-
-
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_6.jpg') }}" alt="" />
-
-                </div>
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_7.jpg') }}" alt="" />
-
-                </div>
-
-                <div class="client-logo">
-
-                    <img src="{{ asset('blood/frontend/images/logo_8.jpg') }}" alt="" />
-
-                </div>
-
-
+                @foreach($sponsors as $sponsor) 
+                    <div class="client-logo">
+                        <img src="{{ asset($sponsor->image) }}" alt="{{ $sponsor->title }}" />
+                    </div>
+                @endforeach
             </div> <!-- end .testimonial-container  -->
-
         </div> <!-- end row  -->
-
     </div> 
 </section> 
 
@@ -286,8 +163,8 @@
                         <i class="fa fa-quote-right" aria-hidden="true"></i>
                     </p>
 
-                    <h6>Brandon Munson</h6>
-                    <span>CTO, Fulcrum Design, USA</span>
+                    <h6>Shahriar Sumon</h6>
+                    <span>CTO, Fulcrum Design, Dhaka, Bangladesh</span>
 
                 </div> <!-- end .testimony-layout-1  -->
 
@@ -303,8 +180,8 @@
                         <i class="fa fa-quote-right" aria-hidden="true"></i>
                     </p>
 
-                    <h6>Brandon Munson</h6>
-                    <span>CTO, Fulcrum Design, USA</span>
+                    <h6>Rabbil Hasan</h6>
+                    <span>MD, Pickme, Bangladesh</span>
 
                 </div> <!-- end .testimony-layout-1  -->
 
@@ -320,30 +197,14 @@
                         <i class="fa fa-quote-right" aria-hidden="true"></i>
                     </p>
 
-                    <h6>Brandon Munson</h6>
-                    <span>CTO, Fulcrum Design, USA</span>
+                    <h6>Tanvir Jalal</h6>
+                    <span>CTO, Digicart Solutions, Dhaka, Bangladesh</span>
 
                 </div> <!-- end .testimony-layout-1  -->
 
             </div> <!--  end col-md-10  --> 
 
         </div>    
-    </div>
-</section> 
-
-<section class="cta-section-1">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                <h2>We are helping people from 40 years</h2>
-                <p>
-                    You can give blood at any of our blood donation venues all over the world. We have total sixty thousands donor centers and visit thousands of other venues on various occasions.                            
-                </p>
-            </div> <!--  end .col-md-8  -->
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-                <a class="btn btn-cta-1" href="#">Request Appointment</a>
-            </div> <!--  end .col-md-4  -->
-        </div> <!--  end .row  -->
     </div>
 </section> 
 @endsection
